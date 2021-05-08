d462b9e194f9beb891bb38488e22195d4f4bab72
KeithSUn <54606160+HRB-NO1@users.noreply.github.com>
2021-05-08 02:09:24 -0700

    Update game_element.cc

---
+++ game_element.cc
@@ -0,0 +1,15 @@
+#include "game_element.h"
+bool GameElement::IntersectsWith(GameElement &gameElement) {
+  return !(GetX() > gameElement.GetX() + gameElement.GetWidth() ||
+           gameElement.GetX() > x_ + GetWidth() ||
+           GetY() > gameElement.GetY() + gameElement.GetHeight() ||
+           gameElement.GetY() > y_ + GetHeight());
+}
+bool GameElement::IsOutOfBounds(const graphics::Image &screen) {
+  if (GetX() + GetWidth() > screen.GetWidth() ||
+      GetY() + GetHeight() > screen.GetHeight() || GetX() < 1 || GetY() < 1) {
+    return true;
+  } else {
+    return false;
+  }
+}
