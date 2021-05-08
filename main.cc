2967f8801cf47c817d977798bd0f71350a3efe39
KeithSUn <54606160+HRB-NO1@users.noreply.github.com>
2021-05-08 02:09:56 -0700

    Update main.cc

---
+++ main.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include "cpputils/graphics/image.h"
+#include "game.h"
+#include "game_element.h"
+#include "opponent.h"
+#include "player.h"
+
+int main() {
+  Game game;
+  game.Init();
+  game.Start();
+
+  return 0;
+}
