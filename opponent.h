896d9fa69e5c8b82dd1bc6782c73bb34eaa5b9a5
KeithSUn <54606160+HRB-NO1@users.noreply.github.com>
2021-05-08 02:10:42 -0700

    Update opponent.h

---
+++ opponent.h
@@ -0,0 +1,42 @@
+#ifndef OPPONENT_H
+#define OPPONENT_H
+
+#include <vector>
+#include "cpputils/graphics/image.h"
+#include "game_element.h"
+
+class OpponentProjectile : public GameElement {
+ public:
+  OpponentProjectile() : OpponentProjectile(0, 0) {}
+
+  OpponentProjectile(int x, int y) : GameElement(x, y, 5, 5) {}
+
+  void Draw(graphics::Image &screen);
+
+  void Move(const graphics::Image &screen);
+
+ private:
+  void PadPoints(std::vector<int> &points, int pad_x, int pad_y);
+
+  const graphics::Color kOrange{255, 165, 0};
+};
+
+class Opponent : public GameElement {
+ public:
+  Opponent() : Opponent(0, 0) {}
+
+  Opponent(int x, int y) : GameElement(x, y, 50, 50) {}
+
+  void Draw(graphics::Image &screen);
+
+  void Move(const graphics::Image &screen);
+
+ private:
+  void PadPoints(std::vector<int> &points, int pad_x, int pad_y);
+
+  const graphics::Color kRed{255, 0, 0};
+  const graphics::Color kOrange{255, 165, 0};
+  const graphics::Color kYellow{255, 255, 0};
+};
+
+#endif
